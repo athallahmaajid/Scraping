9e900640aeebd94f03dd05c492e0ba1256cdf158
athallahmaajid <athallahmaajid@gmail.com>
2021-02-05 22:14:46 +0700

    Scrape Corona Data per 05-02-2021

---
+++ corona_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import requests
+import re
+
+source = requests.get('https://www.worldometers.info/coronavirus/').text
+source = re.sub(r'<.*?>', lambda g: g.group(0).upper(), source)
+
+html = pd.read_html(source)
+
+now = html[0]
+print(now.tail(59))
+now.to_csv('corona_data.csv')
